e883f7c19b280d6c0b6986b5091ec468e22f6faf
WebUnica Chile <javier.millar@gmail.com>
2026-02-11 23:51:48 -0300

    Update car images in FinalCTA

--- src/components/FinalCTA.tsx
+++ src/components/FinalCTA.tsx
@@ -7,8 +7,8 @@ interface FinalCTAProps {
 const FinalCTA: React.FC<FinalCTAProps> = ({ onVerPaquetesClick }) => {
     return (
         <div className="final-cta">
-            <img src="https://templatekit.selaraswp.com/rental/wp-content/uploads/sites/44/2025/10/BMW-XM-SUV-Mockup-01-2.png" alt="" className="car-bg car-left" />
-            <img src="https://templatekit.selaraswp.com/rental/wp-content/uploads/sites/44/2025/10/BMW-XM-SUV-Mockup-01-2.png" alt="" className="car-bg car-right" />
+            <img src="https://templatekit.selaraswp.com/rental/wp-content/uploads/sites/44/2025/10/cta-car-3.png" alt="" className="car-bg car-left" />
+            <img src="https://templatekit.selaraswp.com/rental/wp-content/uploads/sites/44/2025/10/cta-car-4.png" alt="" className="car-bg car-right" />
             <div className="container">
                 <h2 style={{ fontSize: '3rem', fontWeight: 800, marginBottom: '20px' }}>Empieza tu participación hoy</h2>
                 <button className="btn btn-primary" style={{ padding: '18px 45px', fontSize: '1.1rem' }} onClick={onVerPaquetesClick}>Ver paquetes</button>
